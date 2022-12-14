5cb70773797e1a2dbf3566b2a8924b7923648006
xewoiy <matveyjacyn5@yandex.ru>
2022-12-14 19:29:54 +0700

    Upload Site

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function frameget_minfps_fun(){
+  if (document.getElementById("frameget_minfps_div").style.display === "none") {
+    document.getElementById("frameget_minfps_div").style.display = "block";
+  } else {
+    document.getElementById("frameget_minfps_div").style.display = "none";
+  }
+};
+
+function frameget_maxfps_fun(){
+  if (document.getElementById("frameget_maxfps_div").style.display === "none") {
+    document.getElementById("frameget_maxfps_div").style.display = "block";
+  } else {
+    document.getElementById("frameget_maxfps_div").style.display = "none";
+  }
+};
